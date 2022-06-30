30a8f80dbe870e41e688b469d4dee5112bdc2fbd
Tina <tinazamiri7@gmail.com>
2022-06-30 14:31:08 -0400

    added part of the documentation

--- monte-carlo/src/functions.cpp
+++ monte-carlo/src/functions.cpp
@@ -1,3 +1,10 @@
+/// @file functions.cpp
+/// @author Tina Eghdam Zamiri
+/// @date Jun 30, 2022
+/// @brief Module for writing the functions for the 1D Nagel-Schreckenberg traffic flow.
+
+
+
 #include "functions.cpp"
 
 
@@ -8,5 +15,10 @@ float circular_move(float velocity);
 
 
 float increase_velocity(float velocity){
+	return std:min(velocity + 1, MAX);	
+}
+
+float no_accidents(float velocity, float distance){
 	
+
 }
--- monte-carlo/src/functions.h
+++ monte-carlo/src/functions.h
@@ -1,3 +1,9 @@
+/// @file functions.h
+/// @author Tina Eghdam Zamiri
+/// @date Jun 30, 2022
+/// @brief Module for writing the functions for the 1D Nagel-Schreckenberg traffic flow.
+
+
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
