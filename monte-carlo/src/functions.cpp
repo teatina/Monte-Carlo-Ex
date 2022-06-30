cefa710166318a059355f13ca14d87322e0d13f2
Tina <tinazamiri7@gmail.com>
2022-06-30 10:41:28 -0400

    initial commit of the files to write modular code

---
+++ monte-carlo/src/functions.cpp
@@ -0,0 +1,12 @@
+#include "functions.cpp"
+
+
+float increase_velocity(float velocity);
+float no_accidents(float velocity, float distance);
+float random_decrease(float velocity);
+float circular_move(float velocity);
+
+
+float increase_velocity(float velocity){
+	
+}
---
+++ monte-carlo/src/functions.h
@@ -0,0 +1,23 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+#define MAX 5
+
+float increase_velocity(float velocity);
+float no_accidents(float velocity, float distance);
+float random_decrease(float velocity);
+float circular_move(float velocity);
+
+
+
+
+
+#endif
