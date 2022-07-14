1b77598719e331dd46a8b07a9993313aeb55820a
Tina <tinazamiri7@gmail.com>
2022-07-14 00:04:31 -0400

    module for the functions -- needs to be modified and added to

---
+++ MCMM/src/functions.h
@@ -0,0 +1,20 @@
+/// @file functions.h
+/// @author Tina Eghdam Zamiri
+/// @date Julu 01, 2022
+/// @brief Module for writing the functions for monte carlo molecular motion.
+
+
+#pragma once
+
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+
+
+double generate_position();
+
+double total_energy(double energy);
+
+
