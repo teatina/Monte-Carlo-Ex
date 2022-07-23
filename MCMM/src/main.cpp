848b3b3b91d2e4151a0cfda56140a89bbadf247e
Tina <tinazamiri7@gmail.com>
2022-07-23 18:36:25 -0400

    a simple main function to run the module

---
+++ MCMM/src/main.cpp
@@ -0,0 +1,38 @@
+/// @file functions.h
+/// @author Tina Eghdam Zamiri
+/// @date July 23, 2022
+/// @brief Module for executing the functions for monte carlo molecular motion.
+
+
+#inculde <functions.h>
+
+
+
+
+int main(){
+	int molecule_count = 5;
+	int molecule_x[molecule_count], molecule_y[molecule_count];
+	float temperature = 1.0;
+	int num_steps = 5;
+	double energy[num_steps];
+
+	initialization(15);
+	for ( int i = 0; i < molecule_count; i++ ){
+		molecule_x[i] = generate_position();
+		molecule_y[i] = generate_position();
+
+	}
+	
+	energy[0] = system_energy_initialization(molecule_x, molecule_y, molecule_count);
+
+
+	for ( int j = 1; j < num_steps; j++){
+		energy[j] = total_energy(molecule_x, molecule_y, molecule_count, energy[j-1], temperature);
+		cout << "Energy of the system at step << j << "is: " << energy[j];
+	}
+	
+
+	return 0;
+}
+
+
