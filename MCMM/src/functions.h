3fc30f09d656864f467f9964e57e1441fe50a4a4
Tina <tinazamiri7@gmail.com>
2022-07-23 19:02:04 -0400

    some documentation and updated parameters

--- MCMM/src/functions.h
+++ MCMM/src/functions.h
@@ -15,10 +15,31 @@
 
 using namespace std;
 
+
+/// @brief Function to initialize the mersenne twister engine.
 void initialization();
+
+
+/// @brief Function to random generate a number for x or y coordinate of a molecule using the mersenne twister engine.
+/// This function returns a random integer.
 int generate_position();
+
+
+/// @brief Function to calculate the energy of the system at the beginning.
+/// This function return the initial total energy of the system
+/// @param molecules_x the x position of the molecules
+/// @param molecules_y the y position of the molecules
+/// @param count number of molecules
 double system_energy_initialization(int *molecules_x, int *molecules_y,int count);
-double total_energy(int *molecules_x, int *molecules_y, int count, double *energy);
+
+
+/// @brief Function to calculate the energy of the system at every step.
+/// This function return the total energy of the system at a certain step.
+/// @param molecules_x the x position of the molecules
+/// @param molecules_y the y position of the molecules
+/// @param count number of molecules
+/// @param energy the energy of the system at the previous step.
+double total_energy(int *molecules_x, int *molecules_y, int count, double energy);
 
 
 #endif
