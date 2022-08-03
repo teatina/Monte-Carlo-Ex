52005dcb27b7c695af3affae940c2c9fb3a16b1f
Tina <tinazamiri7@gmail.com>
2022-08-03 12:06:00 -0400

    added velocity and position to the class

--- monte-carlo/src/functions.cpp
+++ monte-carlo/src/functions.cpp
@@ -15,46 +15,55 @@ class Car{
 	int num_cars;
 	float probability;
 	double seed;
+	float *velocity;
+	float *position;
 	public: 
-		Car(int numPos, int numSteps, int numCars, float Probability, double Seed): uniform(0,1), {
+		Car(int numPos, int numSteps, int numCars, float Probability, double Seed): uniform(0,1) {
 			num_pos = numPos;
 			num_steps = numSteps;
 			num_cars = numCars;
 			probability = Probability;
 			seed = Seed;	
+			velocity = new float[numCars];
+			positions = new float[numPos];
 		}
 
 		void start(int seed){
 			engine.seed(seed)
 		}
 
-		int random_decrease(float velocity, float probability){
-			if (velocity < 0) return velocity;
-			// generate a random number to move
-			int temp = uniform(engine);
-			// rejection scheme
-			while (temp >= probability){
-				temp = uniform(engine);
-			}
-			velocity -=1;
-			return velocity;
-		}	
-		float increase_velocity(float velocity){
-        		return std:min(velocity + 1, MAX);
-		}		
-
-		float no_accidents(float velocity, float distance){
-       			 return std::min(velocity, distance - 1);
-
-		}	
-
-		float circular_move(float velocity){
-
-		}	
-
+		void run(){
+			for( int i = 0; i < num_cars; i++ ){
+				
+		}
 	private:
 	  std::uniform_int_distribution<int> uniform;
 	  std::mt19937 engine;
+	  int random_decrease(float velocity, float probability){
+          	if (velocity < 0) return velocity;
+                // generate a random number to move
+                int temp = uniform(engine);
+                // rejection scheme
+                while (temp >= probability){
+                	temp = uniform(engine);
+                }
+                velocity -=1;
+                return velocity;
+	   }
+           float increase_velocity(float velocity){
+		 return std:min(velocity + 1, MAX);
+           }
+
+	  float no_accidents(float velocity, float distance){
+		 return std::min(velocity, distance - 1);
+	  }
+
+          float circular_move(float velocity){
+          	return std::min(velocity * 2, velocity);
+	  }
+
+
+
 }
 
 
