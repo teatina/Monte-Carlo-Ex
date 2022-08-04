d5e56ca6126b3dbfeba649b76e1ead0d40a4d19f
Tina <tinazamiri7@gmail.com>
2022-08-03 23:25:29 -0400

    added the run function which is how the simulation is run -- only initializstion of positions and velocity remains

--- monte-carlo/src/functions.cpp
+++ monte-carlo/src/functions.cpp
@@ -33,22 +33,30 @@ class Car{
 		}
 
 		void run(){
-			for( int i = 0; i < num_cars; i++ ){
-				
+			for( int i = 0; i < num_steps; i++ ){
+				for ( int j = 0; j < num_cars; j++ ){
+					velocity[j] = increase_velocity(velocity[j]);
+					float distance = j != (num_cars - 1) ? podition[j+1] - position[j] : 0; 
+					velocity[j] = no_accidents(velocity[j], distance);
+					velocity[j] = random_decrease(velocity[j], probability);
+					position[j] = circular_move(velocity[j], position[j]);
+					cout << "Car number: " << j << " at position: " << position[j] << " with speed: " << velocity[j] << endl; 
+				}
+			}
+		}
+		~Car(){
+			delete[] velocity;
+			delete[] position;
 		}
 	private:
 	  std::uniform_int_distribution<int> uniform;
 	  std::mt19937 engine;
-	  int random_decrease(float velocity, float probability){
-          	if (velocity < 0) return velocity;
+	  float random_decrease(float velocity, float probability){
+          	if (velocity =< 0) return velocity;
                 // generate a random number to move
                 int temp = uniform(engine);
                 // rejection scheme
-                while (temp >= probability){
-                	temp = uniform(engine);
-                }
-                velocity -=1;
-                return velocity;
+                return (temp >= probability) ? velocity - 1 : velocity;
 	   }
            float increase_velocity(float velocity){
 		 return std:min(velocity + 1, MAX);
@@ -58,8 +66,8 @@ class Car{
 		 return std::min(velocity, distance - 1);
 	  }
 
-          float circular_move(float velocity){
-          	return std::min(velocity * 2, velocity);
+          float circular_move(float velocity, float position){
+          	return std::min(velocity + position, position);
 	  }
 
 
